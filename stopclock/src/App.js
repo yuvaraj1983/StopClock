7a6a76cad988d40a61d5240514b69989ade1f2aa
yuvaraj <yuvaraj@coursera-something.com>
2024-05-08 23:09:16 +0530

    Stopwatch completed

---
+++ stopclock/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import StopWatch from "./StopWatch"
+
+function App() {
+  return (
+    <div className="App">
+   <StopWatch />
+    </div>
+  );
+}
+
+export default App;
---
+++ stopclock/src/StopWatch.jsx
@@ -0,0 +1,57 @@
+import React, { useEffect, useState } from 'react'
+
+const StopWatch = () => {
+
+    const [time,setTime] = useState(0);
+    const [isTimerOn, setIsTimerOn] = useState(false);
+
+    const formatTime = (seconds) => {
+        const minutes = Math.floor(seconds / 60);
+        const remainingSeconds = seconds % 60;
+        return `Time: ${minutes}:${remainingSeconds < 10 ? "0" : ""}${remainingSeconds}`;
+      };
+
+    const handleStart = () => {
+        setIsTimerOn((prevValue) => !prevValue);
+    }
+
+    const handleReset = () => {
+        setTime(0);
+        setIsTimerOn(false);
+    }
+
+    useEffect(() => {
+        let timerId
+        if(isTimerOn) {
+            timerId =   setInterval(() => {
+                setTime((prevValue) => prevValue + 1)
+            }, 1000);
+        } else {
+            clearInterval(timerId);
+        }
+
+        return () => clearInterval(timerId)
+     
+    },[isTimerOn])
+
+  return (
+    <div style={{
+        display:'flex',
+        flexDirection:'column',
+        justifyContent:'center',
+        alignItems:'center'
+      
+    }}>
+          <h4>StopWatch</h4>
+          <h4>{formatTime(time)}</h4>
+          <div>
+              <button onClick={handleStart}>{isTimerOn ? 'Stop' : 'Start'}</button>
+              <button onClick={handleReset}>Reset</button>
+          </div>
+        
+    </div>
+  
+  )
+}
+
+export default StopWatch
